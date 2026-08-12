4cb0691fe4e7c25258b2830ecd4848d77414e6c8
nishant5519 <32064093+nishant5519@users.noreply.github.com>
2026-08-12 21:20:51 +0530

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.java
@@ -0,0 +1,17 @@
+class Solution {
+    
+    public int climbStairs(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return fibonacci(n , dp);
+    }
+    private int fibonacci(int n , int[] dp){
+        if(n <= 2)
+            return n;
+
+        if(dp[n]!=-1)
+            return dp[n];
+        
+        return dp[n] = fibonacci(n-1 , dp) + fibonacci(n-2 , dp);
+    }
+}
